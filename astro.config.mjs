f33cafb3d3fdc162b7bd1429878f8fce77d79b61
pkellner <peter@peterkellner.net>
2024-02-25 14:43:59 -0800

    go one two three

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
-
 import vercel from "@astrojs/vercel/serverless";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), markdoc(), keystatic()],
+  integrations: [react(), markdoc(), keystatic(), tailwind()],
   output: 'hybrid',
   adapter: vercel()
 });
